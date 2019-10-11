26d46eef07a1f03db9ccd60f097983239a4a88a2
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-10-11 23:35:09 +0530

    different template for diff routes

--- flaskblog.py
+++ flaskblog.py
@@ -2,20 +2,20 @@ from flask import Flask,render_template
 app = Flask(__name__)
 posts= [
 
-{
-	'author':'Dharmesh ji',
-	'title':'bakchodi ki had',
-	'content': 'Badshah ki gali mein aake uska pata nahi poochte ghulamo ke jhuke hue sarr khud ba khud raasta bata dete hai',
-     'date_posted':'20october'
+         {
+	       'author':'Dharmesh ji',
+	       'title':'bakchodi ki had',
+	       'content': 'Badshah ki gali mein aake uska pata nahi poochte ghulamo ke jhuke hue sarr khud ba khud raasta bata dete hai',
+           'date_posted':'20october'
 
-},
-{
+         },
+         {
 	
-	'author':'abhishek',
-	'title':'cricket',
-	'content': 'Badshah ki gali mein aake uska pata nahi poochte ghulamo ke jhuke hue sarr khud ba khud raasta bata dete hai',
-     'date_posted':'10novemeber'
-}
+	       'author':'abhishek',
+	       'title':'cricket',
+	       'content': 'Badshah ki gali mein aake uska pata nahi poochte ghulamo ke jhuke hue sarr khud ba khud raasta bata dete hai',
+           'date_posted':'10novemeber'
+         }
 
 
 ]
