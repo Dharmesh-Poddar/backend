4ce36ebeaaeb5b21c8c2b046ed5acc50924c62e1
DHARMESH PODDAR <dharmeshpoddar3@gmail.com>
2019-10-13 20:44:06 +0530

    added routes for registeration and login form

--- flaskblog.py
+++ flaskblog.py
@@ -30,3 +30,11 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html',title='About',posts=posts)
+
+@app.route('/register')
+def register():
+    return render_template('register.html',title='Register',form=form)
+
+@app.route('/login')
+def login():
+    return render_template('login.html',title='login',form=form)
